1e80ea0a2867423fd6ef247cf92a5494bb854de8
Mike Raysor <michaelraysor1@gmail.com>
2020-02-07 22:06:25 -0500

    updated CSS, imports, js files

---
+++ starwars/public/characterList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './StarWars.css';
+import Character from './Character';
+
+const CharacterList = (props) => {
+    return (
+        <div className="list-container">
+            <div>{props.value.map(item => <Character classNmae="character-comp" obj={item} key={item.created} />)}</div>
+        </div>
+    )
+}
